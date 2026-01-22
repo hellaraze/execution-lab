97add422270d2bb77d3eadc28560ada720ed8d55
Hellaraze <maksim.21070309@gmail.com>
2026-01-22 09:17:34 +0300

    replay: fix raw_to_eventlog_smoke paths; ignore generated eventlog artifacts

--- replay/tests/raw_to_eventlog_smoke.rs
+++ replay/tests/raw_to_eventlog_smoke.rs
@@ -3,10 +3,11 @@ use std::process::Command;
 #[test]
 fn raw_to_eventlog_smoke() {
     // input is repo-root events_book.log (legacy raw json line)
-    let in_path = "events_book.log";
-    let out_path = "replay/tests/data/_tmp_events_book.eventlog";
+    let in_path = concat!(env!("CARGO_MANIFEST_DIR"), "/../events_book.log");
+    let out_path = concat!(env!("CARGO_MANIFEST_DIR"), "/tests/data/_tmp_events_book.eventlog");
 
     // run converter
+    std::fs::create_dir_all(concat!(env!("CARGO_MANIFEST_DIR"), "/tests/data")).unwrap();
     let status = Command::new("cargo")
         .args([
             "run",
