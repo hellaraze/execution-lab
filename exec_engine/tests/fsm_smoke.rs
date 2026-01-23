954fd27235a000b62560a855061bb4755e95935f
Hellaraze <maksim.21070309@gmail.com>
2026-01-23 13:16:05 +0300

    exec_engine: fix tests for new FSM api

--- exec_engine/tests/fsm_smoke.rs
+++ exec_engine/tests/fsm_smoke.rs
@@ -1,15 +1,18 @@
-use exec_engine::fsm::{apply, OrderEvent, OrderState};
+use exec_engine::fsm::{apply, OrderData, OrderEvent, OrderState};
 
 #[test]
 fn basic_flow() {
-    let mut s = OrderState::New;
+    let mut d = OrderData::new(100);
+    assert_eq!(d.state, OrderState::New);
 
-    s = apply(s, &OrderEvent::Accept);
-    assert_eq!(s, OrderState::Open);
+    apply(&mut d, &OrderEvent::Accept).unwrap();
+    assert_eq!(d.state, OrderState::Open);
 
-    s = apply(s, &OrderEvent::Fill { qty: 1.0 });
-    assert!(matches!(s, OrderState::PartiallyFilled | OrderState::Filled));
+    apply(&mut d, &OrderEvent::Fill { fill_id: 1, qty_atoms: 40 }).unwrap();
+    assert_eq!(d.filled_atoms, 40);
+    assert_eq!(d.state, OrderState::Open);
 
-    s = apply(s, &OrderEvent::Cancel);
-    assert_eq!(s, OrderState::Canceled);
+    apply(&mut d, &OrderEvent::Fill { fill_id: 2, qty_atoms: 60 }).unwrap();
+    assert_eq!(d.filled_atoms, 100);
+    assert_eq!(d.state, OrderState::Filled);
 }
